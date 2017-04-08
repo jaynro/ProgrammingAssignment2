b6879fbad2d89ffa7feef3d6ff03406b9b57b9e0
Jaynro Perez <jaynro@gmail.com>
2017-04-08 15:47:20 -0500

    Assignment for Week 3 on Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  The purpose of below functions is  to minimize computing time 
+## when calculating the inverse of a matrix  by creating  a special object that ## stores a numeric matrix  and caches its inversion. In case it´s requested 
+## again, the result is cached and not computed.
 
-## Write a short comment describing this function
 
+
+##This function creates a special "matrix" object and caches its inverse
+## using solve() function. It`s  always assumed that the matrix its reversible.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## This function computes the inverse of the special object returned 
+## by above functiion. If the inverse has already been calculated, then
+##`cacheSolve`  retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
